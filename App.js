a24dce1a9e36e9a206446ee3522d572b2a4ca127
Sam Gulinello <sg1355@messiah.edu>
2020-04-30 18:27:34 -0400

    fixed errors

--- App.js
+++ App.js
@@ -4,9 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
     return (
     < View style = { styles.container } >
-        <Text > Open up App.js to start working on your app! < /Text> 
-	<Text> Hello iSeek Team</Text>    
-	</View>
+        <Text > Open up App.js to start working on your app! </Text> 
+	    <Text> Hello iSeek Team</Text>    
+    </View>
 );
 }
 
@@ -14,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#b67',
+        backgroundColor: '#b67 ',
         alignItems: 'center',
         justifyContent: 'center',
     },
